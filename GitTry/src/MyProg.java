979a8c75e6bf65b6f0726399a6af5c404833d9f4
Marcin <marcinc2222@gmail.com>
2016-05-08 22:03:15 +0200

    small change in call (2)
    and space added in echo method.

--- GitTry/src/MyProg.java
+++ GitTry/src/MyProg.java
@@ -4,7 +4,7 @@ public class MyProg {
 	public static void main(String[] args) {
 		MyProg mp = new MyProg();
 		System.out.println(mp.multi(4,6));
-		System.out.println(mp.echo("Hello", 5));
+		System.out.println(mp.echo("Hello", 2));
 
 	}
 	public int multi(int a, int b) {
@@ -14,7 +14,7 @@ public class MyProg {
 	public String echo(String input, int x) {
 		String output="";
 		for (int i=0; i<x; i++) {
-			output+=input;
+			output+=input+" ";
 		}
 		return output;
 	}
