9333466d072dd45cde303b7ecaa52686d2f94e12
Marcin <marcinc2222@gmail.com>
2016-05-08 21:52:39 +0200

    Added echo function call

--- GitTry/src/MyProg.java
+++ GitTry/src/MyProg.java
@@ -4,6 +4,7 @@ public class MyProg {
 	public static void main(String[] args) {
 		MyProg mp = new MyProg();
 		System.out.println(mp.multi(4,6));
+		System.out.println(mp.echo("Hello", 4));
 
 	}
 	public int multi(int a, int b) {
