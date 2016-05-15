27176261333404f55b8740c2ce8fd5b4e46b77b5
Marcin <Marcin@Yoda>
2016-05-15 08:49:31 +0200

    one method deleted one method added

--- GitTry/src/MyProg.java
+++ GitTry/src/MyProg.java
@@ -4,19 +4,19 @@ public class MyProg {
 	public static void main(String[] args) {
 		MyProg mp = new MyProg();
 		System.out.println(mp.multi(4,6));
-		System.out.println(mp.echo("Hello", 5));
+		System.out.println(mp.say("Hello"));
 
 	}
+	
+	public String say(String s) {
+		return s+"\b\b";
+	}
+	
 	public int multi(int a, int b) {
 		return a*b;
 	}
 	
-	public String echo(String input, int x) {
-		String output="";
-		for (int i=0; i<x; i++) {
-			output+=input;
-		}
-		return output;
-	}
+	
+	
 
 }
